fc0c2b5fee3be7f17346c09d65f14d59434565af
Олег Матшин <89175955650@mail.ru>
2022-06-19 20:31:40 +0300

    Made speaker.

--- Gazprom_Prod_Scraping_160622.py
+++ Gazprom_Prod_Scraping_160622.py
@@ -2,25 +2,40 @@ import math
 import requests
 from bs4 import BeautifulSoup as bs
 from pprint import pprint
-import datetime
 from tkinter import *
 from tkinter import messagebox
-import time
 from tkinter import simpledialog
+import pyttsx3
 
+
+def speaker_text(text):
+    tts = pyttsx3.init()
+    voices = tts.getProperty('voices')
+    tts.setProperty('voice', 'ru')
+    tts.say(f'{text}')
+    tts.runAndWait()
+    return text
 
 def main_form():
     window = Tk()
     window["bg"] = "black"
     window.title("Python - choice user form.")
-    input_label_ID = Label(text="Нажмите на кнопку исходя из того, что вы намерены сделать!", fg='white', bg='black')
+    text_1 = "Нажмите кнопку!"
+    text_2 = "Привет! Я Алла Пугачёва. Что будем делать?"
+    input_label_ID = Label(text=text_1, fg='white', bg='black')
     input_label_ID.grid(row=1, column=1, padx=5, pady=10, sticky="w")
 
 
     def get_gazprom_suppliers_run():
-        messagebox.showinfo('Начинается создание списка!', 'Закройте это окно и подождите некоторое время!')
+        text_3 = 'Начинается создание списка!'
+        text_4 = 'Закройте это окно и подождите некоторое время!'
+        speaker_text(text_3 + text_4)
+        messagebox.showinfo(text_3, text_4)
         sup_list = get_gazprom_suppliers()
-        messagebox.showinfo('Список поставщиков "Газпрома"', 'Получен!')
+        text_5 = 'Список поставщиков "Газпрома"'
+        text_6 = 'Получен!'
+        speaker_text(text_5 + text_6)
+        messagebox.showinfo(text_5, text_6)
         window.quit()
         wind = Tk()
         text = Text(wind)
@@ -30,18 +45,40 @@ def main_form():
         wind.mainloop()
         wind.quit()
 
-    def suppliers_file_run():
-        messagebox.showinfo('Начинается копирование данных в файл!', 'Закройте это окно и подождите некоторое время!')
-        suppliers_file(filename='gazprom_suppliers_file.txt')
-        messagebox.showinfo(f'Список поставщиков "Газпрома"', 'Успешно записан в файл {filename}.')
-        window.quit()
+    def suppliers_file_run(filename='gazprom_suppliers_file.txt'):
+        text_7 = 'Начинается копирование данных в файл!'
+        text_8 = 'Закройте это окно и подождите некоторое время!'
+        speaker_text(text_7 + text_8)
+        messagebox.showinfo(text_7, text_8)
+        suppliers_file()
+        text_9 = 'Список поставщиков "Газпрома"'
+        text_10 = f'Успешно записан в файл {filename}.'
+        speaker_text(text_9 + text_10)
+        messagebox.showinfo(text_9, text_10)
+        text_11 = 'Форма диалога.'
+        text_12 = "Продолжаем? - нажмите Да, Закончим - нажмите Нет."
+        speaker_text(text_12)
+        ask_form_1 = messagebox.askquestion(text_11, text_12)
+        if ask_form_1 == 'no':
+            window.quit()
 
     def supplier_verification_run():
-        messagebox.showinfo('Начинается создание списка поставщиков "Газпрома"!', 'Закройте это окно и подождите некоторое время!')
+        text_13 = 'Начинается создание списка поставщиков "Газпрома"!'
+        text_14 = 'Закройте это окно и подождите некоторое время!'
+        speaker_text(text_13 + text_14)
+        messagebox.showinfo(text_13, text_14)
         supplier_verification()
-        window.quit()
+        text_15 = 'Проверка поставщиков "Газпрома"'
+        text_16 = 'Проведена и закончена!'
+        speaker_text(text_15 + text_16)
+        messagebox.showinfo(text_15, text_16)
+        text_17 = 'Форма диалога.'
+        text_18 = "Остаться в главном меню? - нажмите Да, Хватит уже - нажмите Нет."
+        speaker_text(text_18)
+        ask_form_2 = messagebox.askquestion(text_17, text_18)
+        if ask_form_2 == 'no':
+            window.quit()
 
-    
     button_1 = Button(text="Получить список поставщиков 'Газпрома'", activebackground='red', highlightcolor='red', bg='blue', fg='white', command=get_gazprom_suppliers_run)
     button_2 = Button(text="Записать список поставщиков 'Газпрома' в файл", activebackground='red', highlightcolor='red', bg='blue', fg='white', command=suppliers_file_run)
     button_3 = Button(text="Проверка на вхождение в список 'Газпрома'", activebackground='red', highlightcolor='red', bg='blue', fg='white', command=supplier_verification_run)
@@ -50,8 +87,8 @@ def main_form():
     button_2.grid(row=4, column=1, padx=10, pady=20)
     button_3.grid(row=5, column=1, padx=10, pady=20)
     button_4.grid(row=10, column=1, padx=10, pady=20, sticky="e")
+    speaker_text(text=text_2)
 
-            
     window.mainloop()
 
 
@@ -74,7 +111,6 @@ def get_gazprom_suppliers(site_link='https://reestr-neftegaz.ru/', url='https://
 
         companies_list = []
         count_companies = 0
-        # print('*' * 100, 'Данные по компаниям: ')
         for num_page in range(1, count_pages + 1):
             url=f'https://reestr-neftegaz.ru/companies/gazprom_mtp/?name=&page={num_page}&collapse=open'
             res_get = requests.get(url, headers=HEADERS)
@@ -83,11 +119,7 @@ def get_gazprom_suppliers(site_link='https://reestr-neftegaz.ru/', url='https://
             for company in companies:
                 count_companies += 1
                 name_company = company.text.strip()
-                # print(count_companies, name_company)
                 companies_list.append([name_company])
-        # print('*' * 100, 'Список компаний: ')
-        # pprint(companies_list)
-        # print('Количество компаний:', len(companies_list))
         return companies_list
 
 def suppliers_file(filename='gazprom_suppliers_file.txt'):
@@ -96,18 +128,26 @@ def suppliers_file(filename='gazprom_suppliers_file.txt'):
         file.write(f'Всего {len(suppliers)} компаний.' + '\n')
         for supplier in suppliers:
             file.write((str(supplier)).replace('[', '').replace(']', '') + '\n')
-    # print(f"The file {filename} was created! {datetime.datetime.now()}")
     return suppliers
 
 def supplier_verification():
     suppliers_list = get_gazprom_suppliers()
     verif_result_list = [[], []]
     while True:
-        sdft = simpledialog.askstring('Ввод названия компании', 'Введите название поставщика, которого нужно проверить в списке поставщиков "Газпрома: "')
+        text_19 = 'Ввод названия компании'
+        text_20 = 'Введите название поставщика, которого нужно проверить в списке поставщиков "Газпрома: "'
+        speaker_text(text_20)
+        sdft = simpledialog.askstring(text_19, text_20)
         if sdft.isdigit():
-            messagebox.showinfo('Ошибка!', 'Вы ввели цифры, попробуйте снова!')
+            text_21 = 'Ошибка!'
+            text_22 = 'Вы ввели цифры, попробуйте снова!'
+            speaker_text(text_21 + text_22)
+            messagebox.showinfo(text_21, text_22)
         else:
-            messagebox.showinfo('Проверка!', f'Вы ввели {sdft} сейчас проверим!')
+            text_23 = 'Проверка!'
+            text_24 = f'Вы ввели {sdft} сейчас проверим!'
+            speaker_text(text_24)
+            messagebox.showinfo(text_23, text_24)
             for supplier in suppliers_list:
                 if sdft.lower() == str(supplier).strip().replace('[', '').replace(']', '').lower():
                     verif_result_list[0].append(supplier)
@@ -115,14 +155,20 @@ def supplier_verification():
                     verif_result_list[1].append(supplier)
             break
     if verif_result_list[0] != []:
-        messagebox.showinfo('Результат проверки!', f'Поставщик {sdft} есть в списке поставщиков "Газпрома"!')
+        text_25 = 'Результат проверки!'
+        text_26 = f'Поставщик {sdft} есть в списке поставщиков "Газпрома"!'
+        speaker_text(text_26)
+        messagebox.showinfo(text_25, text_26)
     elif verif_result_list[0] == [] and verif_result_list[1] != []:
-        messagebox.showinfo('Результат проверки!',
-                        f'Ваш Поставщик: {sdft}. А в списке поставщиков "Газпрома" есть: {verif_result_list[1]}.')
+        text_27 = 'Результат проверки!'
+        text_28 = f'Ваш Поставщик: {sdft}. А в списке поставщиков "Газпрома" есть: {verif_result_list[1]}'
+        speaker_text(text_28)
+        messagebox.showinfo(text_27, text_28)
     elif verif_result_list == [[], []]:
-        messagebox.showinfo('Результат проверки!', f'Поставщика {sdft} нет в списке поставщиков "Газпрома"!')
-    # pprint(suppliers_list)
-    # pprint(verif_result_list)
+        text_29 = 'Результат проверки!'
+        text_30 = f'Поставщика {sdft} нет в списке поставщиков "Газпрома"!'
+        speaker_text(text_30)
+        messagebox.showinfo(text_29, text_30)
     return verif_result_list
 
 
